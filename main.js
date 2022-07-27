d8bb21439383d49d65cf98e33ccebc03c48e3ce2
satyam mishra <satyam.growupnext@gmail.com>
2022-07-27 11:45:16 +0530

    intial commit

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
-function calculate() {
-    var username = document.getElementById("username").value;
-    var weight = document.getElementById("weight").value;
-    var weight_unit = document.getElementById("weight_unit").value;
-    var height = document.getElementById("height").value;
-    var height_unit = document.getElementById("height_unit").value;
+  function calculate() {
+      var username = document.getElementById("username").value;
+      var weight = document.getElementById("weight").value;
+      var weight_unit = document.getElementById("weight_unit").value;
+      var height = document.getElementById("height").value;
+      var height_unit = document.getElementById("height_unit").value;
 
-    console.log(username);
-}
+      if (weight_unit == "1") {
+          weight = Math.round(weight / 2.205); //pound
+          console.log(weight);
+      }
+      if (weight_unit == "2") {
+          weight = weight; //kg
+          console.log(weight);
+
+      }
+      if (weight_unit == "3") {
+          weight = weight / 1000; //gram
+          console.log(weight);
+
+      }
+
+      if (height_unit == "4") {
+          height = Math.round(height / 100); //cm
+          console.log(height);
+      }
+      if (height_unit == "5") {
+          height = height; //meter
+          console.log(height);
+      }
+      if (height_unit == "6") {
+          height = Math.round(height / 3.281); //foot
+          console.log(height);
+      }
+      var BMI_value = Math.round(weight / (height * height));
+      console.log(`Your BMI is ${BMI_value}`);
+      if (BMI_value < 18) {
+          console.log('underweight');
+
+      } else if (BMI_value >= 18 || BMI_value <= 22) {
+          console.log('Weight is OK');
+      } else {
+          console.log('overweight');
+      }
+  }
---
+++ readme.md
@@ -0,0 +1,14 @@
+BMI-Calculator
+
+This is webpage is an small project to calculate your approx. BMI (BODY MASS INDEX).
+
+
+Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women. 
+
+One must know his height in METER and weight in KILOGRAM . 
+
+
+For further details you may visit     https://www.nhp.gov.in/disease/non-communicable-disease/obesity
+You can use the code for your project .
+
+You may checkout my other projects at my GITHUB profile    https://github.com/satyammishra0
